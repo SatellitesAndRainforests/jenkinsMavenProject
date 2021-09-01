99061e19faf28e7acbd495efee4688f4ff088ebd
mark <mark@rakocontrols.com>
2021-09-01 14:53:46 +0100

    Initial commit

---
+++ src/main/java/com/example/folder/test.java
@@ -0,0 +1,12 @@
+package com.example.folder;
+
+/**
+ *@Authour me
+ *@Description of
+ */
+
+public class test {
+    public static void main (String [] args) {
+        System.out.println("hello from jenkins maven project test.main");
+    }
+}

7c5242980c4e798b79528c320d63ed256b805de5
SatellitesAndRainforests <markstart00@googlemail.com>
2021-09-02 09:00:06 +0100

    Update test.java

--- src/main/java/com/example/folder/test.java
+++ src/main/java/com/example/folder/test.java
@@ -7,6 +7,8 @@ package com.example.folder;
 
 public class test {
     public static void main (String [] args) {
+        System.out.println("");
         System.out.println("hello from jenkins maven project test.main");
+        System.out.println("");
     }
 }
